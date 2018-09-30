070264273da2a4e2248807798aadd68d37b33831
zt2534487227 <2534487227@qq.com>
2018-09-30 17:18:58 +0800

    update

--- src/main/java/com/zt/blog/BlogApplication.java
+++ src/main/java/com/zt/blog/BlogApplication.java
@@ -3,14 +3,19 @@ package com.zt.blog;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.zt.blog.dao")
-public class BlogApplication {
+public class BlogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(BlogApplication.class);
+	}
 }
