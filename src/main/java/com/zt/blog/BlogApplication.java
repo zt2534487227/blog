09a83a326590185c4088658dcec1f90b481b70d2
zt2534487227 <2534487227@qq.com>
2018-10-10 20:17:02 +0800

    update

--- src/main/java/com/zt/blog/BlogApplication.java
+++ src/main/java/com/zt/blog/BlogApplication.java
@@ -5,8 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @MapperScan("com.zt.blog.dao")
@@ -21,12 +19,4 @@ public class BlogApplication extends SpringBootServletInitializer {
 		return builder.sources(BlogApplication.class);
 	}
 
-
-	public static class WebConfigurer implements WebMvcConfigurer{
-
-		@Override
-		public void addResourceHandlers(ResourceHandlerRegistry registry) {
-			registry.addResourceHandler("/file/**").addResourceLocations("file://upload");
-		}
-	}
 }
