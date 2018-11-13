56c9aacd06675ef05dcb7123459421188d4b95dc
zhoutian <2534487227@qq.com>
2018-11-13 10:28:39 +0800

    update

---
+++ src/main/java/com/zt/blog/common/config/JsonConfig.java
@@ -0,0 +1,33 @@
+package com.zt.blog.common.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+/**
+ * @author: ZhouTian
+ * @description:
+ * @date: 2018/11/13
+ */
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    @Primary
+    @ConditionalOnMissingBean(ObjectMapper.class)
+    public ObjectMapper jsonObjectMapper(Jackson2ObjectMapperBuilder builder){
+        ObjectMapper objectMapper = builder.createXmlMapper(false).build();
+        // 通过该方法对mapper对象进行设置，所有序列化的对象都将按改规则进行系列化
+        // Include.Include.ALWAYS 默认
+        // Include.NON_DEFAULT 属性为默认值不序列化
+        // Include.NON_EMPTY 属性为 空（""） 或者为 NULL 都不序列化，则返回的json是没有这个字段的。这样对移动端会更省流量
+        // Include.NON_NULL 属性为NULL 不序列化,就是为null的字段不参加序列化
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+        return objectMapper;
+    }
+
+}
