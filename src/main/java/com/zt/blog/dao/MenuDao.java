508c06929a40b1bd3e1dba2e0f76632ddfc8ab97
zhoutian <2534487227@qq.com>
2018-11-13 10:55:46 +0800

    update

--- src/main/java/com/zt/blog/dao/MenuDao.java
+++ src/main/java/com/zt/blog/dao/MenuDao.java
@@ -2,6 +2,7 @@ package com.zt.blog.dao;
 
 import com.zt.blog.model.Menu;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author ZhouTian
  * @since 2018-09-25
  */
+@Repository
 public interface MenuDao extends BaseMapper<Menu> {
 
 }
--- src/main/java/com/zt/blog/dao/UserTokenDao.java
+++ src/main/java/com/zt/blog/dao/UserTokenDao.java
@@ -2,6 +2,7 @@ package com.zt.blog.dao;
 
 import com.zt.blog.model.UserToken;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.springframework.stereotype.Repository;
 
 /**
  * <p>
@@ -11,6 +12,7 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @author ZhouTian
  * @since 2018-11-06
  */
+@Repository
 public interface UserTokenDao extends BaseMapper<UserToken> {
 
 }
